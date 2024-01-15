ebbbe861e581c20d282bf70228e223726e673b03
Alex Grishcchenko <leh.777@mail.ru>
2024-01-15 21:45:54 +0300

    1. В /src создали новую директорию mocks, а в ней новый файл films.ts, а также player.ts. Опишисали в нём тестовые данные для 8-и любых фильмов

---
+++ src/mocks/player.ts
@@ -0,0 +1,5 @@
+import { FilmPlayer } from "src/types";
+
+export const playerMocks: FilmPlayer = {
+    src: 'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4'
+  };
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type FilmCardData = {
+    description: string,
+    director: string,
+    genre: string,
+    id: string,
+    numberOfRatings: number,
+    poster: string,
+    preview: string,
+    raiting: number,
+    starring: string[],
+    title: string,
+    year: number
+  }
+
+  export type FilmPlayer = {
+    src: string
+  }
