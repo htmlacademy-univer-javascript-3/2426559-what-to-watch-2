2bf3c9f7b1100456745e9d25e6a6cb7b153510bc
Alex Grishcchenko <leh.777@mail.ru>
2024-01-16 00:33:53 +0300

    4-5. Создание компонента (Список фильмов). Реализация хранения в стейте информации о наведении на карточку фильма

--- src/components/film-card/film-card.tsx
+++ src/components/film-card/film-card.tsx
@@ -1,24 +1,37 @@
+import { Link } from 'react-router-dom';
 import { FilmCardData } from 'src/types';
+import { RoutePathname } from 'src/constants';
 
-type Props = FilmCardData
+type Props = FilmCardData & {
+  onMouseEnter?: () => void,
+  onMouseLeave?: () => void
+};
 
 export function FilmCard(props: Props) {
-    const {
-        preview,
-        title
-    } = props;
-    return(
-<article
+  const {
+    preview,
+    title,
+    id,
+    onMouseEnter,
+    onMouseLeave
+  } = props;
+  return (
+    <article
       className="small-film-card catalog__films-card"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       <div className="small-film-card__image">
         <img src={preview} alt={title} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href={`/`}>
+        <Link
+          to={`/${RoutePathname.FILMS}/${id}`}
+          className="small-film-card__link"
+        >
           {title}
-        </a>
+        </Link>
       </h3>
     </article>
-    );
+  );
 }
