0ea5126c2b677e7cbb8786de7f281dbfa30be507
Alex Grishcchenko <leh.777@mail.ru>
2024-01-24 01:13:11 +0300

    Определено новое поле authorizationStatus в глобальном состоянии

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,23 @@
 export const enum RoutePathname {
-    MAIN = '/',
-    LOGIN = 'login',
-    MY_LIST = 'mylist',
-    FILMS = 'films',
-    REVIEW = 'review',
-    PLAYER = 'player',
-    NOT_FOUND = '404'
-  }
+  MAIN = '/',
+  LOGIN = 'login',
+  MY_LIST = 'mylist',
+  FILMS = 'films',
+  REVIEW = 'review',
+  PLAYER = 'player',
+  NOT_FOUND = '404'
+}
 
 export enum ReduxStateStatus {
-    pending = 'pending',
-    fulfilled = 'fulfilled',
-    rejected = 'rejected',
-    idle = 'idle'
-  }
+  pending = 'pending',
+  fulfilled = 'fulfilled',
+  rejected = 'rejected',
+  idle = 'idle'
+}
+
+export enum AuthorizationStatus {
+  notAuthorized = 'notAuthorized',
+  authorized = 'authorized'
+}
 
 export const ALL_GENRES = 'All films';
