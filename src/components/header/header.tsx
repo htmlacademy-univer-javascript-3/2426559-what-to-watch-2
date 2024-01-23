720d83254b2e71679187c7c551fb7d5dbc00e4ea
Alex Grishcchenko <leh.777@mail.ru>
2024-01-24 01:34:20 +0300

    Обновление компонента header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,9 @@
-import {JSX} from 'react';
-import {Link} from 'react-router-dom';
-import {RoutePathname} from 'src/constants';
-
+import {JSX, useCallback} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {useSnackbar} from 'notistack';
+import {AuthorizationStatus, ReduxStateStatus, RoutePathname} from 'src/constants';
+import {useAppDispatch, useAppSelector} from 'src/store';
+import {fetchLogout} from 'src/store/api';
 
 type Props = {
   breadcrumbs?: JSX.Element,
@@ -10,6 +12,26 @@ type Props = {
 
 export function Header(props: Props) {
   const {breadcrumbs, headerClass} = props;
+  const navigate = useNavigate();
+  const {enqueueSnackbar} = useSnackbar();
+  const dispatch = useAppDispatch();
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+  const isAuthorized = authorizationStatus === AuthorizationStatus.authorized;
+  const handleLogout = useCallback(() => {
+    dispatch(fetchLogout()).then((res) => {
+      if (res.meta.requestStatus === ReduxStateStatus.rejected) {
+        enqueueSnackbar(
+          'Не удалось выйти',
+          {variant: 'error'}
+        );
+      } else {
+        navigate(RoutePathname.MAIN);
+      }
+    });
+  }, [navigate, dispatch, enqueueSnackbar]);
+  const handleLogin = useCallback(() => {
+    navigate(`/${RoutePathname.LOGIN}`);
+  }, [navigate]);
   return (
     <header className={`page-header ${headerClass ? `${headerClass}` : 'film-card__head'}`}>
       <div className="logo">
@@ -27,7 +49,16 @@ export function Header(props: Props) {
           </div>
         </li>
         <li className="user-block__item">
-          <Link to={`/${RoutePathname.LOGIN}`} className="user-block__link">Sign out</Link>
+        {isAuthorized && (
+            <div className="user-block__link" onClick={handleLogout}>
+              Sign out
+            </div>
+          )}
+          {!isAuthorized && (
+            <div className="user-block__link" onClick={handleLogin}>
+              Sing in
+            </div>
+          )}
         </li>
       </ul>
     </header>
